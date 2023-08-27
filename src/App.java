54d39321f2a1cd4bb8b2dab5e7208f8996dabb72
Bernardo-Zamin <bezamin03@gmail.com>
2023-08-27 16:53:27 -0300

    testes

--- src/App.java
+++ src/App.java
@@ -2,7 +2,14 @@ public class App{
 
     public static void main(String[] args){
         Conta c1 = new Conta.Builder(11, "Alemao").build();
+        
         System.out.println(c1.toString());
+
+        
+        Conta c2 = new Conta.Builder(10,"Bernardo").categoria(Categoria.PREMIUM)
+                            .salAplicacaoInicial(1000).build();
+
+        System.out.println("\n"+c2);
     }
 
 }
